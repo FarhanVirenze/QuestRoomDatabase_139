880f83bbf2f6c00683bbb860fb9b69b3e76307d4
farhan <farhanvirenze18@gmail.com>
2024-12-13 22:04:06 +0700

    penambahan   //getAllMhs                 fun getAllMahasiswa(): Flow<List<Mahasiswa>>                 //getMhs                 fun getMhs(nim: String): Flow<Mahasiswa>                 //deleteMhs                 suspend fun deleteMhs(mahasiswa: Mahasiswa)                 //updateMhs                 suspend fun updateMhs(mahasiswa: Mahasiswa)

--- app/src/main/java/com/pam/questroomdatabase_139/repository/RepositoryMhs.kt
+++ app/src/main/java/com/pam/questroomdatabase_139/repository/RepositoryMhs.kt
@@ -5,5 +5,12 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    //getAllMhs
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+    //getMhs
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    //deleteMhs
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    //updateMhs
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
