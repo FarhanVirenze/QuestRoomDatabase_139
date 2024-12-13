e69625d86471effe94344702c3ded8d776ae8f65
farhan <farhanvirenze18@gmail.com>
2024-12-13 21:40:48 +0700

    perbaikan MainActivity.kt

--- app/src/main/java/com/pam/questroomdatabase_139/MainActivity.kt
+++ app/src/main/java/com/pam/questroomdatabase_139/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pam.questroomdatabase_139.ui.navigation.PengelolaHalaman
 import com.pam.questroomdatabase_139.ui.theme.QuestRoomDatabase_139Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestRoomDatabase_139Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
