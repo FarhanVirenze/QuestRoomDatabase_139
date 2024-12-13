ce786becb106bc90dfeac4d26f08812963ff637c
farhan <farhanvirenze18@gmail.com>
2024-12-13 21:11:58 +0700

    mmebuat ContainerApp.kt

---
+++ app/src/main/java/com/pam/questroomdatabase_139/dependenciesinjection/ContainerApp.kt
@@ -0,0 +1,13 @@
+package com.pam.questroomdatabase_139.dependenciesinjection
+
+import android.content.Context
+
+interface InterfaceContainerApp {
+    val repositoryMhs: RepositoryMhs
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositoryMhs: RepositoryMhs by lazy {
+        LocalRepositoryMhs(KrsDatabase.getDatabase(context).mahasiswaDao())
+    }
+}
