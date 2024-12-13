d24bf6a3a22ce8d64c2fa31055c0dea3b1189279
farhan <farhanvirenze18@gmail.com>
2024-12-13 21:40:27 +0700

    membuat PenyediaViewModel.kt

---
+++ app/src/main/java/com/pam/questroomdatabase_139/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,21 @@
+package com.pam.questroomdatabase_139.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.pam.questroomdatabase_139.KrsApp
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            MahasiswaViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+    }
+}
+
+fun CreationExtras.KrsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
